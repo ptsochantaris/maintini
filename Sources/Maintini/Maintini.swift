8ff286e3db47ac39581f45e4e7312ca4eaac364d
Paul Tsochantaris <ptsochantaris@icloud.com>
2023-08-28 11:41:20 +0100

    Block syntax fix for macOS

--- Sources/Maintini/Maintini.swift
+++ Sources/Maintini/Maintini.swift
@@ -1,7 +1,7 @@
-import Foundation
 import Combine
+import Foundation
 #if !os(macOS)
-import UIKit
+    import UIKit
 #endif
 
 /// An instance of `Maintini`. Note that on macOS all calls are no-ops.
@@ -13,26 +13,26 @@ public enum Maintini {
     /// Maintini.setup()
     /// ```
     public static func setup() {
-#if !os(macOS)
-        if foregroundObserver == nil {
-            foregroundObserver = NotificationCenter.default
-                .publisher(for: UIApplication.willEnterForegroundNotification)
-                .sink { _ in
-                    unPush()
-                    appInBackground = false
-                    endTask()
-                }
-        }
-        if backgroundObserver == nil {
-            backgroundObserver = NotificationCenter.default
-                .publisher(for: UIApplication.didEnterBackgroundNotification)
-                .sink { _ in
-                    appBackgrounded()
-                }
-        }
-#endif
+        #if !os(macOS)
+            if foregroundObserver == nil {
+                foregroundObserver = NotificationCenter.default
+                    .publisher(for: UIApplication.willEnterForegroundNotification)
+                    .sink { _ in
+                        unPush()
+                        appInBackground = false
+                        endTask()
+                    }
+            }
+            if backgroundObserver == nil {
+                backgroundObserver = NotificationCenter.default
+                    .publisher(for: UIApplication.didEnterBackgroundNotification)
+                    .sink { _ in
+                        appBackgrounded()
+                    }
+            }
+        #endif
     }
-    
+
     /// Protect the app from being suspended in the background while the code in the block is executing.
     /// - Parameter block: The code to execute.
     ///
@@ -49,13 +49,11 @@ public enum Maintini {
     ///
     /// Repeated parallel or nested calls are possible, but do note that they will not extend the app's maximum lifetime while in the background.
     public static func maintain(block: () async -> Void) async {
-#if !os(macOS)
         startMaintaining()
         await block()
         endMaintaining()
-#endif
     }
-    
+
     /// Signal that from this point on, if needed, the app should stay active if put into the foreground, as long as iOS will allow.
     ///
     /// ```
@@ -75,16 +73,16 @@ public enum Maintini {
     ///
     /// Repeated parallel start/end sessions are possible, but do note that they will not extend the app's maximum lifetime while in the background.
     public static func startMaintaining() {
-#if !os(macOS)
-        unPush()
-        let count = globalBackgroundCount
-        globalBackgroundCount = count + 1
-        if appInBackground, bgTask == .invalid, count == 0 {
-            appBackgrounded()
-        }
-#endif
+        #if !os(macOS)
+            unPush()
+            let count = globalBackgroundCount
+            globalBackgroundCount = count + 1
+            if appInBackground, bgTask == .invalid, count == 0 {
+                appBackgrounded()
+            }
+        #endif
     }
-    
+
     /// Signal that whatever task needed the app to stay active is done now.
     ///
     /// `Maintini` will start a two second countdown and if there are no other background tasks it will allow the app to be suspended.
@@ -93,55 +91,55 @@ public enum Maintini {
     ///
     /// Start and end calls must always balance out. Consider using a `defer` block for safety, or the `Maintini` block syntax.
     public static func endMaintaining() {
-#if !os(macOS)
-        globalBackgroundCount -= 1
-        if globalBackgroundCount == 0, bgTask != .invalid {
-            push()
-        }
-#endif
+        #if !os(macOS)
+            globalBackgroundCount -= 1
+            if globalBackgroundCount == 0, bgTask != .invalid {
+                push()
+            }
+        #endif
     }
-    
-#if !os(macOS)
-    private static var foregroundObserver: Cancellable?
-    private static var backgroundObserver: Cancellable?
-    private static var bgTask = UIBackgroundTaskIdentifier.invalid
-    private static var globalBackgroundCount = 0
-    private static var appInBackground = UIApplication.shared.applicationState == .background
-    private static let publisher = PassthroughSubject<Void, Never>()
-    private static var cancel: Cancellable?
-    
-    private static func appBackgrounded() {
-        appInBackground = true
-        if globalBackgroundCount != 0, bgTask == .invalid {
-            // log("BG Task starting")
-            bgTask = UIApplication.shared.beginBackgroundTask {
-                endTask()
+
+    #if !os(macOS)
+        private static var foregroundObserver: Cancellable?
+        private static var backgroundObserver: Cancellable?
+        private static var bgTask = UIBackgroundTaskIdentifier.invalid
+        private static var globalBackgroundCount = 0
+        private static var appInBackground = UIApplication.shared.applicationState == .background
+        private static let publisher = PassthroughSubject<Void, Never>()
+        private static var cancel: Cancellable?
+
+        private static func appBackgrounded() {
+            appInBackground = true
+            if globalBackgroundCount != 0, bgTask == .invalid {
+                // log("BG Task starting")
+                bgTask = UIApplication.shared.beginBackgroundTask {
+                    endTask()
+                }
             }
         }
-    }
-    
-    private static func endTask() {
-        if bgTask == .invalid { return }
-        // log("BG Task done")
-        unPush()
-        UIApplication.shared.endBackgroundTask(bgTask)
-        bgTask = .invalid
-    }
-    
-    private static func push() {
-        if cancel == nil {
-            let stride = RunLoop.SchedulerTimeType.Stride(3)
-            cancel = publisher.debounce(for: stride, scheduler: RunLoop.main).sink { _ in
-                cancel = nil
-                endTask()
-            }
+
+        private static func endTask() {
+            if bgTask == .invalid { return }
+            // log("BG Task done")
+            unPush()
+            UIApplication.shared.endBackgroundTask(bgTask)
+            bgTask = .invalid
         }
-        publisher.send()
-    }
-    
-    private static func unPush() {
-        cancel?.cancel()
-        cancel = nil
-    }
-#endif
+
+        private static func push() {
+            if cancel == nil {
+                let stride = RunLoop.SchedulerTimeType.Stride(3)
+                cancel = publisher.debounce(for: stride, scheduler: RunLoop.main).sink { _ in
+                    cancel = nil
+                    endTask()
+                }
+            }
+            publisher.send()
+        }
+
+        private static func unPush() {
+            cancel?.cancel()
+            cancel = nil
+        }
+    #endif
 }
